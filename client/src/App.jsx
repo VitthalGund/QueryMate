2d0596fd30d90aada50c72fdfae7bca1d3d335d7
sahil29P <vitthalgund30@gmail.com>
2023-08-20 13:19:12 +0530

    Initiated client side code

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center text-lg font-bold'>
+        Welcome to Query-Mate
+      </h1>
+    </>
+  )
+}
+
+export default App
