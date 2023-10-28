6b95760ec699e137845c2565d4a9a8b9aad4214f
VitthalGund <vitthalgund30@gmail.com>
2023-10-28 22:23:02 +0530

    Logout functionality is added

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
+import UserContext from '../context/Auth/userContext';
+import { useContext } from 'react';
+import axios from '../api/axios';
 
 const NavBar = () => {
   // const [nav, setNav] = useState(false);
-
+  const { auth, setAuth, setUserData } = useContext(UserContext);
   // const handleNav = () => {
   //   setNav(!nav);
   // };
@@ -11,10 +14,24 @@ const NavBar = () => {
     <div className='flex justify-between items-center h-24 w-full mx-auto px-4 text-white'>
       <h1 className='w-full text-3xl font-bold text-[black]'>QueryMate</h1>
       <ul className='hidden md:flex text-black'>
-        <li><Link to='/' className='p-4'>Home</Link></li>
+        <li><Link to='/' className='p-4 '>Home</Link></li>
         <li><Link to='/about' className='p-4'>About</Link></li>
-        <li><Link to='/loginwithgoogle' className='p-4'>Login</Link></li>
-        <li><Link to='/signupmail' className='p-4'>Signup</Link></li>
+        {!auth ? <>
+          <li><Link to='/loginwithgoogle' className='p-4'>Login</Link></li>
+          <li><Link to='/signupmail' className='p-4'>Signup</Link></li>
+        </> :
+          <li><Link onClick={async () => {
+            if ((await axios.get("/logout", {
+              headers: {
+                Authorization: auth.accessToken
+              }
+            })).status === 204) {
+              alert("Logout SuccessFully")
+              setAuth("");
+              setUserData("")
+            }
+          }} className='p-4'>Logout</Link></li>
+        }
       </ul>
 
     </div>
