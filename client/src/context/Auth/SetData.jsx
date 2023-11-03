8cd6429fd67125f9c9f169fe04d6938b4e91aff2
VitthalGund <vitthalgund30@gmail.com>
2023-11-03 21:41:54 +0530

    modified the context api for chat page The chat is saved in localstorage Remember me value is store in localstorage

--- client/src/context/Auth/SetData.jsx
+++ client/src/context/Auth/SetData.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import UserContext from "./userContext";
-const SetData =(props)=>{
-    const [userData , setUserData]=useState({username:'',email:''});
-    const [auth , setAuth]= useState("");
-    const [rmail , setRmail]=useState();
-    return(
-        <UserContext.Provider value={{userData, setUserData, auth, setAuth, rmail, setRmail}}>
+const SetData = (props) => {
+    const [userData, setUserData] = useState({ username: '', email: '' });
+    const [auth, setAuth] = useState("");
+    const [rmail, setRmail] = useState();
+    const [persist, setPersist] = useState(localStorage.getItem("persist"));
+    useEffect(() => {
+        localStorage.setItem("persist", persist);
+    }, [persist])
+    return (
+        <UserContext.Provider value={{ userData, setUserData, auth, setAuth, rmail, setRmail, setPersist, persist }}>
             {props.children}
         </UserContext.Provider>
     )
--- client/src/context/Chat/ChatID.jsx
+++ client/src/context/Chat/ChatID.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ChatContext from "./useContext";
 
 const ChatID = (props) => {
+    // const [chatId, setChatId] = useState(localStorage.getItem("chatId"));
     const [chatId, setChatId] = useState("653b9d1c69a3c827df961a0e");
     const [multi, setMulti] = useState();
 
+    useEffect(() => {
+        localStorage.setItem("chatId", chatId)
+    }, [chatId])
     return (
         <ChatContext.Provider value={{ chatId, setChatId, multi, setMulti }}>
             {props.children}
