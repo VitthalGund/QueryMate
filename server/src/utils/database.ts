3a8c5a02fed12c64588a9a89722e9587507ecd79
VitthalGund <vitthalgund30@gmail.com>
2023-11-04 22:39:20 +0530

    JWT dependency removed email is taken from request body

--- server/src/utils/database.ts
+++ server/src/utils/database.ts
@@ -5,23 +5,21 @@ import natural from "natural";
 import { UserChat } from "../models/Chat.js";
 import express from "express";
 
-import jwt, { JwtPayload } from "jsonwebtoken";
-
 // Function to save text data to MongoDB
 export async function saveToMongoDB(
   text: string,
   req: express.Request,
-  chatId: mongoose.Types.ObjectId
+  chatId: mongoose.Types.ObjectId,
+  email?: string
 ) {
   let chunks: string[] = [];
-
-  if (!useModel) {
-    await loadUseModel();
-  }
-
   // console.log(text);
   // if (Buffer.from(text).length > 16 * 1024 * 1024) {
   if (text.length >= 4000) {
+    if (!useModel) {
+      await loadUseModel();
+    }
+
     // Split the text into chunks if it exceeds 16MB
     chunks = await chunkPassage(
       text,
@@ -34,10 +32,6 @@ export async function saveToMongoDB(
     chunks.push(text);
   }
 
-  const res: JwtPayload = jwt.verify(
-    req.headers.authorization,
-    process.env.REFRESH_TOKEN_SECRET!
-  ) as JwtPayload;
   // console.log(chunks);
   const chatResp = [];
   let multi = false;
@@ -50,8 +44,8 @@ export async function saveToMongoDB(
       const chat = new UserChat({
         chatId: chatId,
         title: text.slice(0, 10),
-        email: res.decoded["email"],
-        fileName: req.file.originalname || " ",
+        email,
+        fileName: req.file?.originalname || " ",
         passage: chunk,
         multi,
       });
