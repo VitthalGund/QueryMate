eb51506b469def061718411ce5498ab9f596ad70
VitthalGund <vitthalgund30@gmail.com>
2023-11-04 22:38:33 +0530

    POST /chats route modified email filter is added before sending chatId

--- server/src/routes/message.ts
+++ server/src/routes/message.ts
@@ -15,18 +15,27 @@ router.post("/", async (req: express.Request, res: express.Response) => {
   res.json({ messages: response });
 });
 
-router.get("/chats", async (req: express.Request, res: express.Response) => {
+router.post("/chats", async (req: express.Request, res: express.Response) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({
+      error: "Missing email!",
+    });
+  }
+
   const response = await UserChat.distinct("chatId");
 
   const chats = [];
   for (let index = 0; index < response.length; index++) {
     const element = response[index];
-    chats.push(
-      await UserChat.findOne(
-        { chatId: element },
-        { chatId: 1, fileName: 1, title: 1 }
-      )
-    );
+    if (element.email) {
+      chats.push(
+        await UserChat.findOne(
+          { chatId: element },
+          { chatId: 1, fileName: 1, title: 1 }
+        )
+      );
+    }
   }
   res.json({ chats });
 });
