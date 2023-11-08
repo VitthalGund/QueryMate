09fdc4d6e1376605babd4e71b2ee636edd0c3d46
VitthalGund <vitthalgund30@gmail.com>
2023-11-08 10:47:53 +0530

    POST /messages/edit route added This route provide user to edit the title of their chats

--- server/src/routes/message.ts
+++ server/src/routes/message.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ChatMessage } from "../models/message.js";
 import { UserChat } from "../models/Chat.js";
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
 router.post("/", async (req: express.Request, res: express.Response) => {
@@ -15,28 +16,61 @@ router.post("/", async (req: express.Request, res: express.Response) => {
   res.json({ messages: response });
 });
 
+router.post("/edit", async (req: express.Request, res: express.Response) => {
+  const { newTitle, chatId } = req.body;
+  if (!newTitle) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Missing new title" });
+  }
+  if (!chatId) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Missing new title" });
+  }
+  const Response = jwt.verify(
+    req.headers.authorization,
+    process.env.ACCESS_TOKEN_SECRET
+  );
+  const resp = await UserChat.findOne({
+    chatId,
+    email: Response["UserInfo"].email,
+  });
+  if (!resp) {
+    return res
+      .status(400)
+      .json({ message: "invalid credentails", success: false });
+  }
+
+  resp.title = newTitle;
+  const done = await resp.save();
+  res.status(202).json({
+    message: "title updated successfully!",
+    success: true,
+    response: done,
+  });
+});
+
 router.post("/chats", async (req: express.Request, res: express.Response) => {
   const { email } = req.body;
+  console.log(email);
   if (!email) {
     return res.status(400).json({
       error: "Missing email!",
     });
   }
 
-  const response = await UserChat.distinct("chatId");
-
+  const response = await UserChat.find({ email }).distinct("chatId");
+  console.log(response);
   const chats = [];
   for (let index = 0; index < response.length; index++) {
-    const element = response[index];
-    if (element.email) {
-      chats.push(
-        await UserChat.findOne(
-          { chatId: element },
-          { chatId: 1, fileName: 1, title: 1 }
-        )
-      );
-    }
+    const element = await UserChat.findOne(
+      { chatId: response[index] },
+      { passage: 0, multi: 0 }
+    );
+    chats.push(element);
   }
+  console.log(chats);
   res.json({ chats });
 });
 
