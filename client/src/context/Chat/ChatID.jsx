7ccb11b376497962257b0a01f3f74ee5e46eef65
VitthalGund <vitthalgund30@gmail.com>
2023-11-08 10:51:40 +0530

    chats message are added in chat contex api This allow the access and modification of message across all components

--- client/src/context/Chat/ChatID.jsx
+++ client/src/context/Chat/ChatID.jsx
@@ -2,15 +2,29 @@ import { useEffect, useState } from "react";
 import ChatContext from "./useContext";
 
 const ChatID = (props) => {
-    // const [chatId, setChatId] = useState(localStorage.getItem("chatId"));
-    const [chatId, setChatId] = useState("653b9d1c69a3c827df961a0e");
+    const [chatId, setChatId] = useState(localStorage.getItem("chatId"));
+    // const [chatId, setChatId] = useState("653b9d1c69a3c827df961a0e");
     const [multi, setMulti] = useState();
+
+    function formatDate(date) {
+        const h = "0" + date.getHours();
+        const m = "0" + date.getMinutes();
+
+        return `${h.slice(-2)}:${m.slice(-2)}`;
+    }
+    const [info, setInfo] = useState([{
+        mate: "QueryMate",
+        text: "Hello! I am your QueryMate assistant.\n I am here to help you with any question you may have regarding the provided curpus",
+        date: formatDate(new Date()),
+        img: `https://api.multiavatar.com/QueryMate.png`,
+        side: "start"
+    }]);
 
     useEffect(() => {
         localStorage.setItem("chatId", chatId)
     }, [chatId])
     return (
-        <ChatContext.Provider value={{ chatId, setChatId, multi, setMulti }}>
+        <ChatContext.Provider value={{ chatId, setChatId, multi, setMulti, info, setInfo, formatDate }}>
             {props.children}
         </ChatContext.Provider>
     );
