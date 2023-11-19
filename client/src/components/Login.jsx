f10a07571846117c7845f2decfe21b8be98c70f7
VitthalGund <vitthalgund30@gmail.com>
2023-11-19 21:06:39 +0530

    greay background added

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -75,15 +75,15 @@ export default function LogIn() {
             success ?
                 <div></div> :
                 <>
-                    <div className="font-serif mt-3">
+                    <div className="font-serif  bg-slate-50">
                         <Link to="/loginwithgoogle" className="ml-4 text-3xl">←</Link>
                     </div>
-                    <div className="flex justify-center items-center flex-col h-[90vh]">
+                    <div className="flex justify-center items-center flex-col h-[90vh] bg-slate-50">
                         <div className="ml-0 mr-0 ">
                             <p className="text-2xl font-bold mb-3">LOG IN</p>
                         </div>
                         <form onSubmit={handleOnSubmit}
-                            className="flex flex-col justify-center items-center font-serif bg-white p-4 rounded-lg">
+                            className="flex flex-col justify-center items-center font-serif bg-slate-50 p-4 rounded-lg">
                             <div className="relative bg-inherit mt-3 mb-5">
                                 <input type="email"
                                     id="email" name="email"
@@ -94,7 +94,7 @@ export default function LogIn() {
                                     className="peer bg-transparent h-10 w-72 rounded-lg text-black placeholder-transparent ring-2 px-2 ring-gray-500 focus:ring-sky-600 focus:outline-none focus:border-rose-600"
                                 />
                                 <label htmlFor="email"
-                                    className="absolute cursor-text left-0 -top-3 text-sm text-gray-500 bg-inherit mx-1 px-1 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-sky-600 peer-focus:text-sm transition-all"
+                                    className="absolute cursor-text left-0 -top-3 text-lg text-gray-500 bg-inherit mx-1 px-1 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-sky-600 peer-focus:text-sm transition-all"
                                 >Email</label>
                             </div>
                             <div className="relative bg-inherit mt-3 mb-5">
@@ -108,7 +108,7 @@ export default function LogIn() {
                                     className="peer bg-transparent h-10 w-72 rounded-lg text-black placeholder-transparent ring-2 px-2 ring-gray-500 focus:ring-sky-600 focus:outline-none focus:border-rose-600"
                                 />
                                 <label htmlFor="password"
-                                    className="absolute cursor-text left-0 -top-3 text-sm text-gray-500 bg-inherit mx-1 px-1 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-sky-600 peer-focus:text-sm transition-all"
+                                    className="absolute cursor-text left-0 -top-3 text-lg text-gray-500 bg-inherit mx-1 px-1 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-sky-600 peer-focus:text-sm transition-all"
                                 >Password</label>
                             </div>
                             <label htmlFor="showPassword" className="mb-4">
