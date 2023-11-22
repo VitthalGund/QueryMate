8df666f4c73b77bb6be792e06363a7b303691d58
VitthalGund <vitthalgund30@gmail.com>
2023-11-22 18:06:29 +0530

    some border, shadow and hover effects are added

--- client/src/components/StudentInfo.jsx
+++ client/src/components/StudentInfo.jsx
@@ -4,7 +4,7 @@ export function StudentInfo(props) {
     return (
         <>
             <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg bg-white shadow-indigo-500">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-xl py-8 px-7 shadow-md transition-all hover:shadow-lg sm:p-9 lg:px-6 xl:px-9">
                     <img alt="team" className="w-24 h-24 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={props.imgLink} />
                     <div className="flex-grow">
                         <h2 className="text-gray-900 title-font font-medium text-1xl">{props.name}</h2>
