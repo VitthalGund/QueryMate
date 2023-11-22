9074da47f1ebdc807e57e747e3166013706419d0
VitthalGund <vitthalgund30@gmail.com>
2023-11-22 18:02:03 +0530

    home page link added in 404 page

--- client/src/components/Page404.jsx
+++ client/src/components/Page404.jsx
@@ -1,6 +1,8 @@
+import { useNavigate } from "react-router-dom"
 
 
 const Page404 = () => {
+    const naviagte = useNavigate();
     return (
         <>
             <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
@@ -13,7 +15,9 @@ const Page404 = () => {
                                     doorway to the great nothing
                                 </h1>
                                 <p className="my-2 text-gray-800">Sorry about that! Please visit our hompage to get where you need to go.</p>
-                                <button className="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">Take me there!</button>
+                                <button className="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50"
+                                    onClick={() => naviagte("/")}
+                                >Take me there!</button>
                             </div>
                         </div>
                         <div>
