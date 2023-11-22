65218e14852adb040475029386f348773e4ee6a7
VitthalGund <vitthalgund30@gmail.com>
2023-11-22 18:05:37 +0530

    our team section brief intro line added

--- client/src/components/OurTeam.jsx
+++ client/src/components/OurTeam.jsx
@@ -5,8 +5,9 @@ const OurTeam = () => {
             <section className="text-gray-700 body-font border-t border-gray-200 bg-gray-50">
                 <div className="container px-5 py-20 mx-auto">
                     <div className="flex flex-col text-center w-full mb-20">
-                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-indigo-600">Our Team</h1>
-                        {/* <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven&apos;t heard of them.</p> */}
+                        <h2 className="text-4xl xl:text-5xl capitalize text-center text-indigo-900 font-bold">our team</h2>
+                        <hr className="mx-auto w-12 h-1 outline-0 border-0 bg-indigo-300 block mt-4 mb-6" />
+                        <p className="text-center text-xl text-gray-800">Our team consists only of the best talents</p>
                     </div>
                     <div className="flex flex-wrap -m-2 items-center justify-center">
                         <StudentInfo imgLink="https://avatars.githubusercontent.com/u/97181033?v=4" name='Vitthal Gund' role='Full Stack Developer' github='https://github.com/VitthalGund' linkedin='https://in.linkedin.com/in/vitthal-gund' />
