f5784c39327413dca1fafc75853d8a62675dd2a3
VitthalGund <vitthalgund30@gmail.com>
2023-11-27 12:19:14 +0530

    logout functionality added in sider

--- client/src/components/Sider.jsx
+++ client/src/components/Sider.jsx
@@ -250,7 +250,7 @@ const Sider = () => {
                 </div>
                 <div
                     className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-                    onClick={toHome}
+                    onClick={() => logout()}
                 >
                     <i className="bi bi-box-arrow-in-right"></i>
                     <span
